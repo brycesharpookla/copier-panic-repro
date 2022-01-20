688290540771f230a8a639c0f3137d9f55101b45
Bryce Sharp <bryce.sharp@ookla.com>
2022-01-20 12:23:20 -0600

    Updated to minimum repro

--- main.go
+++ main.go
@@ -3,26 +3,22 @@ package main
 import (
 	"fmt"
 	"github.com/jinzhu/copier"
-	"google.golang.org/api/androidpublisher/v3"
-	"google.golang.org/api/googleapi"
-	"net/http"
 )
 
 func main() {
 	fmt.Println("-- Start --")
 
-	product := androidpublisher.InAppProduct{
-		ServerResponse: googleapi.ServerResponse{
-			Header: http.Header{
-				"Content-Type": []string{"application/json"}, // Removing this line fixes it. Still happens if slice is empty
-			},
-		},
+	// map[string][]string is the type of the http.Header in googleapi.ServerResponse on the thing we're tyring to clone googleapi.InAppProduct
+	// Its anonymous on the InAppProduct, but thats not the issue as seen in non-anonymous example
+	// https://pkg.go.dev/google.golang.org/api/androidpublisher/v3#InAppProduct
+	httpHeader := map[string][]string{
+		"Content-Type": []string{}, // Removing this line fixes it. Still happens if slice is empty
 	}
+	httpHeaderTarget := &map[string][]string{}
 
-	productTarget := &androidpublisher.InAppProduct{}
-	cloneErr := copier.CopyWithOption(productTarget, product, copier.Option{DeepCopy: true})
+	cloneErr := copier.CopyWithOption(httpHeaderTarget, httpHeader, copier.Option{DeepCopy: true})
 	if cloneErr != nil {
-		fmt.Println(fmt.Sprintf("Clone Error: %s", cloneErr))
+		fmt.Println(fmt.Sprintf("httpHeader Clone Error: %s", cloneErr))
 	}
 
 	fmt.Println("-- Fin --")
